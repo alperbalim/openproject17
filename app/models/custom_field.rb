9bf280513c4a786316af3a67f38072c2194f4368
Ivan Kuchin <i.kuchin@openproject.com>
2025-12-15 20:38:44 +0100

    allow comments for all field formats
    https://community.openproject.org/projects/stadt-koeln/work_packages/64264/activity#comment-1607511

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -361,7 +361,7 @@ class CustomField < ApplicationRecord
   end
 
   def can_have_comment?
-    is_a?(ProjectCustomField) && field_format.in?(%w[bool date float int list user version])
+    is_a?(ProjectCustomField)
   end
 
   ##
